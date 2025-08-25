8344b2f70ddb78372591a53b333de0e9f0459ee1
Rafael <rafaelacerda@protonmail.com>
2025-08-24 23:40:46 -0300

    Add the bloch sphere prototype and the required packages

--- labweb/src/pages/simulacoes/App.jsx
+++ labweb/src/pages/simulacoes/App.jsx
@@ -1,6 +1,8 @@
 
 import '../../styles/style.css';
 import React from "react";
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls} from "@react-three/drei";
 
 function Recicla() {
 
@@ -17,6 +19,18 @@ function Recicla() {
                 <div className='info'>
                     <h1>Simule operações de portas quânticas com uma Esfera de Bloch</h1>
                 </div>
+                <div className='sphereContainer'>
+                    <Canvas camera={{position:[3,2,3], fov:45}}>
+                        <OrbitControls />
+                        <ambientLight intensity={0.2}></ambientLight>
+                        <directionalLight insensity={3} position={[2, 5, 1]}></directionalLight>
+                        <mesh position={[0,0,0]}>
+                            <sphereGeometry args={[1, 32, 32]}></sphereGeometry>
+                            <meshStandardMaterial color="white" transparent={true} opacity={0.9} roughness={0.3}></meshStandardMaterial>
+                        </mesh>
+                    </Canvas>
+                </div>
+
 
             </div>
         </div>
